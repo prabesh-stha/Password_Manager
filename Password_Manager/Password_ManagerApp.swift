f1da0260cdc4ae05488001df01c9c8f1b63bee89
Prabesh Shrestha <shresthaprabesh155@gmail.com>
2024-07-24 17:45:53 +0545

    Initial Commit

---
+++ Password_Manager/Password_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_ManagerApp.swift
+//  Password_Manager
+//
+//  Created by Prabesh Shrestha on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Password_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
